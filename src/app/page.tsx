8719386947869c39fad187729e186f5bc5ed45ee
AsianIntel <gautam.abhyankar@gmail.com>
2024-01-14 22:24:54 -0800

    update profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,65 @@
-import Profile from "../../public/profile.jpg";
 import RoWifi from "../../public/rowifi.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col py-24 gap-y-12 w-full lg:w-2/3 mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2 justify-items-center">
-        <Image
-          src={Profile}
-          alt="Profile"
-          className="rounded-full"
-          width={200}
-          height={200}
-          priority
-        />
+      <div className="grid grid-cols-1 gap-y-2 justify-items-center">
         <div className="flex flex-col gap-y-4 whitespace-nowrap">
-          <span className="text-2xl md:text-4xl lg:text-5xl text-neutral-200">Gautam Abhyankar</span>
-          <span className="text-neutral-700 font-bold">@AsianIntel</span>
+          <span className="text-2xl font-semibold text-neutral-200 text-center">
+            Gautam Abhyankar
+          </span>
+          <span className="text-neutral-700 font-bold text-center">
+            @AsianIntel
+          </span>
           <span className="text-neutral-200">
-            Your Local Discord Bot Developer
+            Passionate CS student with experience in Frontend, Backend, Infra &
+            Product Development
           </span>
         </div>
+      </div>
+
+      <div className="flex gap-x-2 items-center justify-center">
+        <a href="https://linkedin.com/in/asianintel">
+          <svg
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            className="h-8 w-8"
+          >
+            <path
+              fill="#0A66C2"
+              d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"
+            />
+          </svg>
+        </a>
+        <a href="https://twitter.com/AsianIntel_">
+          <svg
+            viewBox="0 0 1200 1227"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+          >
+            <path
+              d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
+              fill="white"
+            />
+          </svg>
+        </a>
+        <a href="https://github.com/AsianIntel">
+          <svg
+            viewBox="0 0 98 96"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+              fill="white"
+            />
+          </svg>
+        </a>
       </div>
 
       <div className="flex flex-col px-8 md:px-24">
@@ -35,7 +74,7 @@ export default function Home() {
               className="h-6 w-6"
             >
               <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
-              <g stroke="#61dafb" stroke-width="1" fill="none">
+              <g stroke="#61dafb" strokeWidth="1" fill="none">
                 <ellipse rx="11" ry="4.2" />
                 <ellipse rx="11" ry="4.2" transform="rotate(60)" />
                 <ellipse rx="11" ry="4.2" transform="rotate(120)" />
@@ -87,16 +126,18 @@ export default function Home() {
         <span className="text-xs text-neutral-400 font-semibold uppercase tracking-widest">
           What have I done?
         </span>
-        <div className="grid grid-cols-3 mt-8">
-          <Image
-            src={RoWifi}
-            alt="RoWifi Icon"
-            className="rounded-full col-span-1"
-            width={150}
-            height={150}
-          />
-          <div className="col-span-2 px-4 flex flex-col gap-y-2">
-            <span className="text-xl font-semibold">RoWifi</span>
+        <div className="flex flex-col mt-8">
+          <div className="flex flex-col gap-y-2">
+            <span className="text-xl font-semibold flex items-center gap-x-2">
+              <Image
+                src={RoWifi}
+                alt="RoWifi Icon"
+                className="rounded-full col-span-1"
+                width={25}
+                height={25}
+              />
+              RoWifi
+            </span>
             <span className="text-xs font-bold text-neutral-600 uppercase tracking-widest">
               June 2019 - Present
             </span>
@@ -106,17 +147,29 @@ export default function Home() {
               seamless server management. Currently serving 60k+ servers and 2M+
               members.
             </span>
-            <span className="text-neutral-200">
-              Codebase - 5 repositories spanning 44k+ LOC
-            </span>
+            <div>
+              <a
+                href="https://github.com/RoWifi-HQ"
+                className="text-blue-600 underline underline-offset-2"
+              >
+                Github
+              </a>
+              <span> | </span>
+              <a
+                href="https://rowifi.xyz"
+                className="text-blue-600 underline underline-offset-2"
+              >
+                Website
+              </a>
+            </div>
             <div className="flex flex-col">
               <span className="text-base font-semibold tracking-wide text-neutral-400">
                 Tech Stack:{" "}
               </span>
               <span>
                 Rust, Astro, TailwindCSS, React, RadixUI, Typescript, Axum,
-                PostgreSQL, Google Cloud, Promethrus, Grafana, Nomad, Consul,
-                Cloudflare, Redis
+                PostgreSQL, Google Cloud, Nomad, Consul, Cloudflare, Redis,
+                Sentry
               </span>
             </div>
             <div className="flex flex-col">
@@ -154,6 +207,36 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <div className="flex flex-col mt-16">
+          <div className="col-span-2 flex flex-col gap-y-2">
+            <span className="text-xl font-semibold">Hue Hustlers</span>
+            <span className="text-xs font-bold text-neutral-600 uppercase tracking-widest">
+              August 2023 - December 2023
+            </span>
+            <div>
+              <a
+                href="https://github.com/athanggupte/CS-526-Game-Project"
+                className="text-blue-600 underline underline-offset-2"
+              >
+                Github
+              </a>
+              <span> | </span>
+              <a
+                href="https://athanggupte.github.io/Hue-Hustle/"
+                className="text-blue-600 underline underline-offset-2"
+              >
+                Game
+              </a>
+            </div>
+            <span className="text-neutral-200">
+              Gameplay programmer/Level Designer/Animator development of a
+              visually captivating platformer game as part of the CS526 Game
+              Project, implementing innovative color state changes for the
+              player.
+            </span>
+          </div>
+        </div>
       </div>
 
       <div className="flex flex-col px-8 md:px-24">
@@ -176,7 +259,7 @@ export default function Home() {
             <span className="text-neutral-200">
               Masters - Computer Science
               <br />
-              Current GPA - 4.00/4.00
+              Current GPA - 3.95/4.00
             </span>
             <div className="flex flex-col">
               <span className="text-base font-semibold tracking-wide text-neutral-500">
@@ -187,8 +270,9 @@ export default function Home() {
                 <li>CS420 Computer Graphics</li>
                 <li>CS561 Foundations of AI</li>
                 <li>CS522 Game Engine Development</li>
-                <li>CS576 Multimedia Systems Design (Currently Studying)</li>
+                <li>CS576 Multimedia Systems Design</li>
                 <li>CS526 Advanced Mobile Devices and Game Consoles</li>
+                <li>CS520 Computer Animation & Simulation</li>
               </ul>
             </div>
           </div>
@@ -209,23 +293,13 @@ export default function Home() {
               <br />
               GPA - 9.66/10
             </span>
-            <div className="flex flex-col">
-              <span className="text-base font-semibold tracking-wide text-neutral-500">
-                Notable Courses:{" "}
-              </span>
-              <ul className="list-disc list-inside">
-                <li>CS312 Database Management System</li>
-                <li>CS331 System Software and Compilers</li>
-                <li>CS324 High Performance Computing</li>
-                <li>CSP42C Cloud Computing</li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
 
       <span className="text-neutral-300 text-center mt-12">
-        @ 2023 Gautam.A<br/>
+        @ 2023 Gautam.A
+        <br />
         All images and logos are trademarks or copyrights of their respective
         owners
       </span>
